ab45e98be6d3b421fdd966e28a88c76f652689a7
Jonas Lundahl <jonas.lundahl@outlook.com>
2021-09-08 17:01:40 +0200

    Set up project and Firebase settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDAwY8qRVvYsc9VyhMubR9i1QEsMDCF7Hc",
+    authDomain: "oshop-212d4.firebaseapp.com",
+    projectId: "oshop-212d4",
+    storageBucket: "oshop-212d4.appspot.com",
+    messagingSenderId: "12555148141",
+    appId: "1:12555148141:web:c7db692116565807c51864",
+    measurementId: "G-ZWLX30HRCV"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDAwY8qRVvYsc9VyhMubR9i1QEsMDCF7Hc",
+    authDomain: "oshop-212d4.firebaseapp.com",
+    projectId: "oshop-212d4",
+    storageBucket: "oshop-212d4.appspot.com",
+    messagingSenderId: "12555148141",
+    appId: "1:12555148141:web:c7db692116565807c51864",
+    measurementId: "G-ZWLX30HRCV"
+  }
 };
