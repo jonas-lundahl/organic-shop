bf77ea8dd1c1a2737eecfe2e740eb1eee3427ec3
Jonas Lundahl <jonas.lundahl@outlook.com>
2021-09-09 18:19:16 +0200

    Hide/Show admin links
    .switchMap in conjection with the async pipe in our navbar can cause infinite loops in the change detection feature of Angular. This can freeze your browser entirely. Be wary of this when working with nested Observables!
    
    Note that we do not need to unsubscribe from the appUser observable in the NavBar component because it is always visible throughout our entire application.

--- src/app/admin-auth-guard.service.ts
+++ src/app/admin-auth-guard.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from './auth.service';
-import 'rxjs/add/operator/switchMap'
 import { UserService } from './user.service';
 
 @Injectable()
@@ -11,8 +10,7 @@ export class AdminAuthGuard implements CanActivate {
   constructor(private auth: AuthService, private userService: UserService) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.auth.user$
-      .switchMap(user => this.userService.get(user.uid))
+    return this.auth.appUser$
       .map(appUser => appUser.isAdmin);
   }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -3,14 +3,18 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase';
 import { ActivatedRoute } from '@angular/router';
+import { AppUser } from './models/app-user';
+import { UserService } from './user.service';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
+  constructor(private userService: UserService, private afAuth: AngularFireAuth, private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
-   }
+  }
 
   login() {
     let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
@@ -20,6 +24,14 @@ export class AuthService {
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+
+  get appUser$(): Observable<AppUser> {
+    return this.user$
+      .switchMap(user => {
+        if (user) return this.userService.get(user.uid);
+        else return Observable.of(null);
+      });
   }
 
 }
