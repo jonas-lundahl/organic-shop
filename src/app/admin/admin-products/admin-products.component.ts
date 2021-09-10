e3deffb32c90a69a639b876a4e53c6c05492c1d7
Jonas Lundahl <jonas.lundahl@outlook.com>
2021-09-10 18:03:23 +0200

    Extract Product interface
    Doing this helps with compile-time checking.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Product } from '../../models/product';
 import { ProductService } from '../../product.service';
 
 @Component({
@@ -8,8 +9,8 @@ import { ProductService } from '../../product.service';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
-  products: any[];
-  filteredProducts: any[];
+  products: Product[];
+  filteredProducts: Product[];
   subscription: Subscription;
 
   constructor(private productService: ProductService) {
