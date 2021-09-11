d181164866c245be366f4ca97414e3abf88b7630
Jonas Lundahl <jonas.lundahl@outlook.com>
2021-09-11 11:55:33 +0200

    Filter products by category
    When you refresh the page, no products are displayed. This is remedied in the next commit.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from '../category.service';
+import { Product } from '../models/product';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -8,12 +10,22 @@ import { ProductService } from '../product.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   categories$;
+  category: string;
 
-  constructor(productService: ProductService, private categoryService: CategoryService) { 
-    this.products$ = productService.getAll();
+  constructor(route: ActivatedRoute, productService: ProductService, private categoryService: CategoryService) {
+    productService.getAll().subscribe(products => this.products = products);
     this.categories$ = categoryService.getAll();
+
+    // We cannot use snapshot here because we are not going to another page/component.
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get('category');
+      this.filteredProducts = (this.category) ?
+        this.products.filter(p => p.category === this.category) :
+        this.products;
+    })
   }
 
 }
